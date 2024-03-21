a07012956b8df9d18fd0a9dae6e6c078f5b3f1e7
lrahbek <laura.rahek@gmail.com>
2024-03-21 21:52:04 +0100

    commit almost finished a3

---
+++ cds-lang-assignment-3/run.sh
@@ -0,0 +1,7 @@
+
+# activate the environment
+source ./env/bin/activate
+# run the code
+python src/keywordcounter.py "$@"
+# close the environment
+deactivate
--- cds-lang-assignment-3/src/keywordcounter.py
+++ cds-lang-assignment-3/src/keywordcounter.py
@@ -2,52 +2,45 @@ import os
 import pandas as pd 
 import gensim.downloader as api
 from collections import Counter
+import string
 import argparse
-
-
 
 def get_arguments():
     """
     Get input arguments for query expansion; artist and keyword
     """
     parser = argparse.ArgumentParser()
-
     parser.add_argument(
                         "--artist",
                         "-a", 
                         required = True,
                         help="The artist should be given as a string")
-
     parser.add_argument(
                         "--keyword",
                         "-k", 
                         required = True,
                         help="The keyword shoule be given in a string")                    
-
     args = parser.parse_args()
     return args
 
 
-
-def artist_find(artist_q):
+def artist_find(artist_input):
     """
     The function loads in the Spotify Million Song Dataset and takes a string as input, 
     if the string matches with an artist name in the corpus, a list with the given 
-    artists' songs is returned.
+    artist's songs is returned.
     """
-    filepath = os.path.join("..", "in", "Spotify Million Song Dataset_exported.csv")
+    filepath = os.path.join("in", "Spotify Million Song Dataset_exported.csv")
     songs = pd.read_csv(filepath)
 
-    artist_q = artist_q.lower()
+    artist = artist_input.lower()
     songs["artist"] = (songs["artist"]).str.lower()
-    
-    if (songs['artist'].eq(artist_q).any()):
-        artist_cor = songs[songs["artist"] == artist_q]
-        artist_songs = list(artist_cor["text"])
+
+    if (songs['artist'].eq(artist).any()):
+        artist_corpus = songs[songs["artist"] == artist]
+        artist_songs = list(artist_corpus["text"])
     else: print('Artist not found') 
-
     return artist_songs
-
 
 
 def query_expansion(keyword):
@@ -57,31 +50,48 @@ def query_expansion(keyword):
     the function returns as a list. 
     """
     model = api.load("glove-wiki-gigaword-50")
-
     query = model.most_similar(keyword)
     query_ls = ([i[0] for i in query])
     query_ls.append(keyword)
-
     return query_ls
 
-
-
-def keyword_count(keyword, song_texts, artist_q):
+def clean_tokens(text):
     """
-    The function takes a keyword and a list of song texts, it returns the number
-    of songs in the input list that contains any of the words in the keyword list. 
+    The function takes a text as an input tokenizes it, makes it lowercase and 
+    strips the puctuation from all words. It returns a list of the cleaned tokens.
+    """
+    tokens = text.lower().split()
+    tokens_cleaned = []
+    for token in tokens: 
+        strip_punct = token.strip(string.punctuation)
+        if not strip_punct == "":
+            tokens_cleaned.append(strip_punct)
+    return tokens_cleaned
+
+def keyword_count(keyword, song_texts, artist_input):
+    """
+    The function takes a keyword and a list of song texts, it returns the percentage 
+    of the given songs that contains any of the 10 words mostly related to the keyword,
+    and the keyword, of the number of songs from that artist. 
     """
     query_c = Counter(query_expansion(keyword))
     song_count = 0
 
-    for text in range(len(song_texts)):     
-        text_c = Counter(song_texts[text].lower().split())
-        keyword_count = (text_c & query_c).total()
+    for text in range(len(song_texts)):
+        tokens_cleaned = clean_tokens(song_texts[text])       
+        tokens_c = Counter(tokens_cleaned)
+        keyword_count = (tokens_c & query_c).total()
         if keyword_count > 0: 
             song_count +=1
+
     songs_perc = round(((song_count/len(song_texts))*100), 2)
 
-    return print(f"{songs_perc}% of {artist_q}'s songs contains words related to {keyword}")    
+    return print(f"{songs_perc}% of {artist_input}'s songs contains words related to {keyword}")    
+
+def save_output(keyword):
+    keywords_output = open("keywords.txt", "a")
+    keywords_output.write(query_expansion(keyword))
+    keywords_output.close()
 
 
 def main():
