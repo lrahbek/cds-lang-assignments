95ef22b5276a1ab29efd0f3edad92f1e0fc396c2
lrahbek <98777027+lrahbek@users.noreply.github.com>
2024-05-20 15:07:34 +0200

    Update setup.sh

--- assignment-5/setup.sh
+++ assignment-5/setup.sh
@@ -1,12 +1,5 @@
-#make virtual environment
-python -m venv env
-#open virtual environment
-source ./env/bin/activate
 #find dependencies and install requirements 
 pip install --upgrade pip
-pip install pipreqs
-pipreqs src --savepath requirements.txt
 pip install -r requirements.txt
-#exit the virtual environment
-deactivate
+
 
