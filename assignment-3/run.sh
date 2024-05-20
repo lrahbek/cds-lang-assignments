4bd0dae8b4ecb4b9a7855a9c5e2faa275853d313
lrahbek <98777027+lrahbek@users.noreply.github.com>
2024-05-20 12:20:21 +0200

    Update run.sh

--- assignment-3/run.sh
+++ assignment-3/run.sh
@@ -1,6 +1,6 @@
 #open the virtual environment 
 source ./env/bin/activate
-# run the code keywordcounter.py script
+#run the keywordcounter.py script
 python src/keywordcounter.py "$@"
 #exit environment 
 deactivate
